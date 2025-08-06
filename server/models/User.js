57c9da0744dcb8f54537de4c03f98ca9bc1a1e72
Ajay rawat <ajjuujja07@gmail.com>
2025-08-06 12:51:21 +0530

    user edit

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
-    role: { type: String, enum: ['vendor', 'admin'], default: 'vendor' }
+    role: { type: String, enum: ['user', 'vendor', 'admin'], default: 'vendor' }
 });
 
 module.exports = mongoose.model('User', userSchema);
